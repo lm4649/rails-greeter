dbe47cf1ea77314982f72036e7d6cd090f15a473
marc lopes <marc_lopes2004@yahoo.fr>
2020-08-18 09:17:16 +0900

    cards and contributions destroyed along with their parent user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable
-  has_many :cards
-  has_many :contributions
+  has_many :cards, dependent: :destroy
+  has_many :contributions, dependent: :destroy
 end
