097846aa2b82379233225fdd1c0a34f712aa7385
marc lopes <marc_lopes2004@yahoo.fr>
2020-08-18 13:31:40 +0900

    completed the cards controller methods

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,16 +1,38 @@
 class CardsController < ApplicationController
   def new
+    @card = Card.new
   end
 
   def create
+    @card = Card.new(card_params)
+    @card.user = current_user
+    if @card.save
+      redirect_to edit_card(@card)
+    else
+      render :new
+    end
   end
 
   def edit
+    @card = Card.find(params[:id])
   end
 
   def update
+    @card = Card.find(params[:id])
+    if @card.update(card_params)
+      redirect_to card_path(@card)
+    else
+      render :edit
+    end
   end
 
   def show
+    @card = Card.find(params[:id])
+  end
+
+  private
+
+  def card_params
+    params.require(:card).permit(:title, :recipient, :event_date, :description, :draft)
   end
 end
