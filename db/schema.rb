5579021833cd125bf0a5f29385b5e6decf94c62e
marc lopes <marc_lopes2004@yahoo.fr>
2020-08-18 12:08:37 +0900

    added description column to cards table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_104036) do
+ActiveRecord::Schema.define(version: 2020_08_18_030708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_08_17_104036) do
     t.boolean "draft", default: true
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
     t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
