b76829643305a0fc0d90199bdb9618e92c3e8843
marc lopes <marc_lopes2004@yahoo.fr>
2020-08-18 13:03:44 +0900

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'contributions/new'
-  get 'cards/new'
-  get 'cards/edit'
-  get 'cards/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cards, only: [:new, :create, :show, :edit, :update] do
+    resources :contributions, only: [:new, :create]
+  end
+  resources :contributions, only: :update
+  get '/dashboard', to:'users#dashboard'
 end
