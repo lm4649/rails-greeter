27d9f2ef5c471c557be3eed2c97f958422d021c5
Michelle Macato <michellefmacato@gmail.com>
2020-08-20 12:23:36 +0900

    not yet finish

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,10 +25,13 @@ require("channels")
 // External imports
 import "bootstrap";
 
+
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { initBanner} from './components/banner';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initBanner();
 });
