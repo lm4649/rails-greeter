1d7812655a857d56ca3eb5b0aa02c613e5cc588f
marc lopes <marc_lopes2004@yahoo.fr>
2020-08-24 13:58:45 +0900

    photo attached to card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -5,6 +5,7 @@ class Card < ApplicationRecord
   validates :event_date, presence: true
   validates :recipient_email, allow_blank: true, format: { with: /\A.*@.*\.com\z/ }
   validate :event_date_in_future
+  has_one_attached :photo
 
   def event_date_in_future
     errors.add(:event_date, 'must be in the future') if Date.today > event_date
