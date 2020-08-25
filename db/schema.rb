6d36eca46881e3a943c94e99528f666b28bef757
marc lopes <marc_lopes2004@yahoo.fr>
2020-08-25 15:33:06 +0900

    schema again

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_031733) do
+ActiveRecord::Schema.define(version: 2020_08_25_060745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,8 +45,8 @@ ActiveRecord::Schema.define(version: 2020_08_24_031733) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
-    t.string "template"
     t.string "recipient_email"
+    t.string "template"
     t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
