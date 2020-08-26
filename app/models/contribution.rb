955f0109b7984d2531e13c39f601292e6e6ac9d7
marc lopes <marc_lopes2004@yahoo.fr>
2020-08-26 09:29:31 +0900

    changed email regex for card and contribution models

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -2,7 +2,7 @@ class Contribution < ApplicationRecord
   belongs_to :card
   belongs_to :user, optional: true
   validates :contributor_name, presence: true, length: { minimum: 2 }
-  validates :contributor_email, allow_blank: true, allow_nil: true, format: { with: /\A.*@.*\.com\z/ }
+  validates :contributor_email, allow_blank: true, allow_nil: true, format: { with: /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/i }
   has_one_attached :photo
 
   def rejected?
