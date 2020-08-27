021bcdb928c7749f3e2649b78702cddd853ec255
marc lopes <marc_lopes2004@yahoo.fr>
2020-08-27 11:24:32 +0900

    added picture on the contribution form as well

--- app/javascript/packs/components/update_upload_btn.js
+++ app/javascript/packs/components/update_upload_btn.js
@@ -7,15 +7,15 @@ const updateUploadBtn = () => {
         // uploadBtn.classList.remove('d-none');
         const icon = document.querySelector('label i');
         icon.classList.remove('fa-camera');
-        icon.classList.add('fa-check-circle');
-        icon.classList.add('validation-color');
+        // icon.classList.add('fa-check-circle');
+        // icon.classList.add('validation-color');
       }
       else {
         //dealing with corner cases
         const icon = document.querySelector('label i');
         if (!icon.classList.contains('fa-camera')) icon.classList.add('fa-camera');
-        if (icon.classList.contains('fa-check-circle'))icon.classList.remove('fa-check-circle');
-        if (icon.classList.contains('validation-color'))icon.classList.remove('validation-color');
+        // if (icon.classList.contains('fa-check-circle'))icon.classList.remove('fa-check-circle');
+        // if (icon.classList.contains('validation-color'))icon.classList.remove('validation-color');
       }
     })
 
