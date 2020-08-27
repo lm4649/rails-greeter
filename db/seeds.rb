934f60c024d308abaeb4f887d9739b1a4acef3a1
marc lopes <marc_lopes2004@yahoo.fr>
2020-08-27 13:44:40 +0900

    trying to seed pics from url

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
+require 'open-uri'
 
 puts 'desrtoying user, cards and contributions'
 # cards and contribution are destroyed along with their user
@@ -13,9 +14,9 @@ User.destroy_all
 
 puts 'create 3 users : Micky, Marc and Samir'
 
-User.create!(email: 'Micky@lewagon.com', password: 'greeter1234')
-User.create!(email: 'Samir@lewagon.com', password: 'greeter1234')
-User.create!(email: 'Marc@lewagon.com', password: 'greeter1234')
+User.create!(email: 'micky@lewagon.com', password: 'greeter1234')
+User.create!(email: 'samir@lewagon.com', password: 'greeter1234')
+User.create!(email: 'marc@lewagon.com', password: 'greeter1234')
 
 puts "#{User.count} users created"
 
@@ -24,11 +25,18 @@ puts 'adding cards and contributions for each user'
 User.all.each do |user|
   # adding 10 cards to each user
   10.times do
-    card = Card.create!(user: user, title: "#{Faker::Name.unique.first_name}'s birthday", event_date: Date.today + rand(1..15), description: "explaining here for who and for what event")
+    titles = ["#{Faker::Name.unique.first_name}'s birthday", "Happy Wedding #{Faker::Name.unique.male_first_name} and #{Faker::Name.unique.female_first_name}", "Merry Christmas #{Faker::Name.unique.male_first_name} and #{Faker::Name.unique.female_first_name}"]
+    picked_title = titles.sample
+    date = titles.index(picked_title) == 2 ? Date.new(2020,12,25) : Date.today + rand(1..15)
+    card = Card.create!(user: user, title: picked_title, event_date: date, description: "explaining here for who and for what event", recipient_email: user.email)
     # adding 5 contributions to each card, the 1st one is the card manager contribution
     Contribution.create!(user: user, card: card, contributor_name: user.email, content: Faker::Lorem.paragraph(sentence_count: 5))
     24.times do
-      Contribution.create!(card: card, contributor_name: Faker::Name.unique.first_name, content: Faker::Lorem.paragraph(sentence_count: 5))
+      contribution = Contribution.new(card: card, contributor_name: Faker::Name.unique.first_name, content: Faker::Lorem.paragraph(sentence_count: 5) )
+      url = "https://randomuser.me/api/portraits/#{['men', 'women'].sample}/#{rand(1...100)}.jpg"
+      puts URI.parse(url)
+      contribution.photo.key = URI.parse(url)
+      contribution.save
     end
   end
 end
